eaa2df817451e0380a629fbf377bfb3413fb5fa4
Vitor Sousa <vitorsousavhcs@outlook.com>
2021-04-02 23:04:30 +0100

    Adding some components to the dashboard base

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# dependencies
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -7,35 +8,21 @@ import plotly.express as px
 import geopandas
 import numpy as np
 
-
 from geojson import FeatureCollection
 from shapely.geometry import mapping
 from dash.dependencies import Input, Output, State
 from simpledbf import Dbf5
 
+from static_graphs import contact_type_graph, age_by_sex_graph
+##
 
+df = px.data.tips()
+fig = px.pie(df, values='tip', names='day', color_discrete_sequence=px.colors.sequential.RdBu)
+
+##
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
-
-# the style arguments for the sidebar. We use position:fixed and a fixed width
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "22rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-CONTENT_STYLE = {
-    "margin-left": "23rem",
-    "margin-right": "2rem",
-    "padding": "5rem 1rem",
-}
 
 sidebar = html.Div(
     [
@@ -55,9 +42,8 @@ sidebar = html.Div(
             pills=True,
         ),
     ],
-    style=SIDEBAR_STYLE,
+    className="sidebar"
 )
-
 
 def get_data():
 
@@ -85,9 +71,9 @@ def get_data():
 
 df_data, geojson = get_data()
 
-content = html.Div(id="page-content", style=CONTENT_STYLE)
-
-app.layout = html.Div([dcc.Location(id="url"), dcc.Store(id="memory_output", data={"value": "nr_crimes"}), sidebar, content])
+content = html.Div(id="page-content", className="content")
+app.layout = html.Div([dcc.Location(id="url"), dcc.Store(id="memory_output", data={"value": "nr_crimes"}), sidebar,
+                       content])
 
 dropdown = dcc.Dropdown(
     id="drop_down1",
@@ -128,7 +114,8 @@ def render_page_content(pathname, data):
     if pathname == "/":
         return ""
     elif pathname == "/page-1":
-        return html.P("")
+        gen_view = get_general_view()
+        return gen_view
     elif pathname == "/cartographic_view":
         fig = build_fig(metric="incidencia")
         return update_content(fig)
@@ -138,6 +125,159 @@ def render_page_content(pathname, data):
 def on_dropdown_select(value):
     return {"value": value}
 
+def get_general_view():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.H1(
+                                "APAV", style={"font-weight": "bold"}
+                            ),
+                            html.H2(
+                                "Relatório Anual 2020", style={"margin-top": "0px", "font-weight": "bold"}
+                            ),
+                        ]
+                    )
+                ],
+                className="one-half column",
+                id="title",
+            ),
+            html.H3(
+                "DESTAQUES", style={"margin-top": "0px", "font-weight": "bold"}
+            ),
+            html.Div(
+                [
+
+                    html.Div(
+                        [html.H4(66.408, className="mini_container_text"),
+                         html.P("ATENDIMENTOS", className="mini_container_label")],
+                        id="attendance",
+                        className="mini_container"
+
+                    ),
+                    html.Div(
+                        [html.H4(19.697, className="mini_container_text"),
+                         html.P("CRIMES & OUTRAS FORMAS DE VIOLÊNCIA", className="mini_container_label")],
+                        id="crime",
+                        className="mini_container"
+                    ),
+                    html.Div(
+                        [html.H4(13.093, className="mini_container_text"),
+                         html.P("VÍTIMAS", className="mini_container_label")],
+                        id="victims",
+                        className="mini_container"
+                    ),
+                    html.Div(
+                        [html.H4(1.227, className="mini_container_text"),
+                         html.P("ATIVIDADES FORMATIVAS", className="mini_container_label")],
+                        id="formativeactivies",
+                        className="mini_container"
+                    ),
+                ],
+                id="info-container",
+                className="row container-display",
+            ),
+            html.H3(
+                "NÚMERO MÉDIO DE VÍTIMAS", style={"margin-top": "0px", "font-weight": "bold"}
+            ),
+            html.Div(
+                [
+
+                    html.Div(
+                        [html.H4("MULHERES", className="mini_container_label"),
+                         html.P([html.B(8720), " ANO"]), html.P([html.B(167), " Semana"]),
+                         html.P([html.B(24), " Dia"])],
+                        id="woman_number",
+                        className="mini_container",
+                        style={"background-color": "#ff000033",
+                               "background-image": "url(assets/women.png)",
+                               "background-repeat": "no-repeat",
+                               "background-size": "30%",
+                               "background-position-y": "bottom",
+                               "background-position-x": "right",
+                               }
+                    ),
+                    html.Div(
+                        [html.H4("CRIANÇAS", className="mini_container_label"),
+                         html.P([html.B(1841), " ANO"]), html.P([html.B(35), " Semana"]),
+                         html.P([html.B(5), " Dia"])],
+                        id="children_number",
+                        className="mini_container",
+                        style={"background-color": "#ff000033",
+                               "background-image": "url(assets/children_1.png)",
+                               "background-repeat": "no-repeat",
+                               "background-size": "30%",
+                               "background-position-y": "bottom",
+                               "background-position-x": "right",
+                               }
+                    ),
+                    html.Div(
+                        [html.H4("HOMENS", className="mini_container_label"),
+                         html.P([html.B(1627), " ANO"]), html.P([html.B(31), " Semana"]),
+                         html.P([html.B(4), " Dia"])],
+                        id="men_numbers",
+                        className="mini_container",
+                        style={"background-color": "#ff000033",
+                               "background-image": "url(assets/men.png)",
+                               "background-repeat": "no-repeat",
+                               "background-size": "30%",
+                               "background-position-y": "bottom",
+                               "background-position-x": "right",
+                               }
+                    ),
+                    html.Div(
+                        [html.H4("IDOSOS", className="mini_container_label"),
+                         html.P([html.B(1626), " ANO"]), html.P([html.B(31), " Semana"]),
+                         html.P([html.B(4), " Dia"])],
+                        id="formativeactivies",
+                        className="mini_container",
+                        style={"background-color": "#ff000033",
+                               "background-image": "url(assets/senior.png)",
+                               "background-repeat": "no-repeat",
+                               "background-size": "30%",
+                               "background-position-y": "bottom",
+                               "background-position-x": "right",
+                               }
+                    ),
+                ],
+                id="info-container_2",
+                className="row container-display",
+            ),
+            html.H3(
+                "NÚMERO MÉDIO DE VÍTIMAS", style={"margin-top": "0px", "font-weight": "bold"}
+            ),
+            html.Div(
+                [
+                    html.Div(
+                        [dcc.Graph(id="conctact_type_graph", figure=contact_type_graph)],
+                        className="pretty_container three columns",
+                    ),
+                    html.Div(
+                        [dcc.Graph(id="age_by_sex_graph", figure=age_by_sex_graph)],
+                        className="pretty_container three columns",
+                    ),
+                    html.Div(
+                        [dcc.Graph(id="conctact_type_graph", figure=contact_type_graph)],
+                        className="pretty_container three columns",
+                    ),
+                    html.Div(
+                        [dcc.Graph(id="conctact_type_graph", figure=contact_type_graph)],
+                        className="pretty_container three columns",
+                    )
+                ], className="row"),
+            html.Footer(
+                [
+                    html.B("Fonte: "),
+                    html.A("https://apav.pt/apav_v3/images/pdf/Estatisticas_APAV_Relatorio_Anual_2020.pdf")
+                ],
+                className="footer"
+            )
+        ],
+        id="right-column",
+        className="eight columns",
+    )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ static_graphs.py
@@ -0,0 +1,85 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import copy
+
+mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
+layout = dict(
+    autosize=True,
+    automargin=True,
+    margin=dict(l=30, r=30, b=20, t=40),
+    hovermode="closest",
+    plot_bgcolor="#F9F9F9",
+    paper_bgcolor="#F9F9F9",
+    legend=dict(font=dict(size=10), orientation="h")
+)
+
+layout_pie = copy.deepcopy(layout)
+
+data = [
+    dict(
+        type="pie",
+        labels=["Telefónico", "Presencial", "Apoio Online/Email"],
+        values=[61.6,19.5, 17.7],
+        name="TIPO DE CONTACTO",
+        text=[
+            "TELEFÓNICO",
+            "PRESENCIAL",
+            "APOIO ONLINE/EMAIL",
+        ],
+        hoverinfo="text+percent",
+        textinfo="label+percent+name",
+        hole=0.5,
+        marker=dict(colors=["#fac1b7", "#a9bb95", "#92d8d8"])
+    )
+]
+layout_pie["title"] = "TIPO DE CONTACTO"
+
+layout_pie["font"] = dict(color="#777777")
+layout_pie["legend"] = dict(
+    font=dict(color="#CCCCCC", size="10"), orientation="h", bgcolor="rgba(0,0,0,0)"
+)
+
+contact_type_graph = dict(data=data, layout=layout_pie)
+
+
+
+
+women_bins = np.array([-600, -623, -653, -650, -670, -578, -541, -411, -322, -230])
+men_bins = np.array([600, 623, 653, 650, 670, 578, 541, 360, 312, 170])
+
+y = list(range(0, 100, 10))
+
+layout_2 = go.Layout(yaxis=go.layout.YAxis(title='Idade'),
+                     margin=dict(l=30, r=30, b=20, t=40),
+                   xaxis=go.layout.XAxis(
+                       range=[-1200, 1200],
+                       tickvals=[-1000, -700, -300, 0, 300, 700, 1000],
+                       ticktext=[1000, 700, 300, 0, 300, 700, 1000],
+                       title='Nº de Casos'),
+                   barmode='overlay')
+
+data_2 = [go.Bar(y=y,
+               x=men_bins,
+               orientation='h',
+               name='Homens',
+               hoverinfo='x',
+               marker=dict(color='blue')
+               ),
+        go.Bar(y=y,
+               x=women_bins,
+               orientation='h',
+               name='Mulheres',
+               text=-1 * women_bins.astype('int'),
+               hoverinfo='text',
+               marker=dict(color='red')
+               )]
+layout_2["title"] = "CASOS POR SEXO/IDADE"
+
+layout_2["font"] = dict(color="#777777")
+age_by_sex_graph = (dict(data=data_2, layout=layout_2))
